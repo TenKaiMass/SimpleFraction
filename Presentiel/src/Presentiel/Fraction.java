156aae0d1d3b5f4ae1471a0529c37aa25ed98d2e
Massine Merouane <massine.merouane@ens.uvsq.fr>
2020-09-28 16:37:39 +0200

    changement de lieu de depot local

---
+++ Presentiel/src/Presentiel/Fraction.java
@@ -0,0 +1,12 @@
+package Presentiel;
+
+public class Fraction {
+	
+	public String s;
+	
+	public String toString() {
+		return s;
+		
+	}
+
+}
---
+++ Presentiel/src/Presentiel/Main.java
@@ -0,0 +1,10 @@
+package Presentiel;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Fraction toString = new Fraction();
+
+	}
+
+}
