b95a28866a9162f7cfd913adf4fc40f1ced45b12
Massine Merouane <massine.merouane@ens.uvsq.fr>
2020-10-01 15:23:06 +0200

    ajout constructeur et methode

--- Presentiel/src/Presentiel/Fraction.java
+++ Presentiel/src/Presentiel/Fraction.java
@@ -1,12 +1,17 @@
 package Presentiel;
 
 public class Fraction {
-	
-	public String s;
-	
+	public String s1;
+	public String s2;
+
+	public Fraction (String s1, String s2) {
+		this.s1 = s1;
+		this.s2 = s2;	
+	}
 	public String toString() {
-		return s;
-		
+		String s;
+		s = this.s1 + "/" + this.s2;
+		return s;	
 	}
 
 }
--- Presentiel/src/Presentiel/Main.java
+++ Presentiel/src/Presentiel/Main.java
@@ -3,8 +3,10 @@ package Presentiel;
 public class Main {
 
 	public static void main(String[] args) {
-		Fraction toString = new Fraction();
-
+		String s1, s2;
+		s1 = "9";
+		s2 = "3";
+		Fraction frac = new Fraction(s1, s2);
+		System.out.println(frac.toString());
 	}
-
 }
